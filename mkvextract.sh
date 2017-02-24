c8dd9b6a2643901b763d0671798230bed8206391
Chaowei Shan <cwshan@aliyun.com>
2017-02-24 19:55:07 +0800

    add ssa

--- mkvextract.sh
+++ mkvextract.sh
@@ -2,7 +2,7 @@
 /Applications/MKVToolNix-9.8.0.app/Contents/MacOS/mkvinfo $1|grep -E 'Track|Name'
 echo "Please enter the track number to extract"
 read track_number
-echo "Please enter the track format (srt/ass)"
+echo "Please enter the track format (srt/ass/ssa)"
 read format
 /Applications/MKVToolNix-9.8.0.app/Contents/MacOS/mkvextract tracks $1 $track_number:./tracks$track_number.$format
 
